e19c4a08be4631d3b9372e0641c8f134ceda9c05
Anupamkush9 <anupam.kush9@gmail.com>
2023-05-01 10:35:59 +0530

    Dynamic api with database integration

--- orders_api/create.py
+++ orders_api/create.py
@@ -1,7 +1,14 @@
 import json
+import boto3
+import os
 
 def lambda_handler(event, context):
     order = json.loads(event['body'])
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDER_TABLE')
+    table = dynamodb.Table(table_name)
+    response = table.put_item(TableName=table_name, Item=order)
+    print("response: ", response)
     return {
         'statusCode': 201,
         'headers': {},
--- orders_api/read.py
+++ orders_api/read.py
@@ -1,9 +1,23 @@
-import json
+import simplejson as json
+import boto3, os
+from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    order = {'id':123, 'name':'this is test order'}
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDER_TABLE')
+    table = dynamodb.Table(table_name)
+    order_id = int(event['pathParameters']['id'])
+    response = table.query(
+                    KeyConditions={
+                        'id': {
+                            'AttributeValueList': [order_id],
+                            'ComparisonOperator': 'EQ'
+                        }
+                    }
+                )
+
     return {
         'statusCode': 200,
         'headers': {},
-        'body': json.dumps(order)
+        'body': json.dumps(response['Items'])
     }
